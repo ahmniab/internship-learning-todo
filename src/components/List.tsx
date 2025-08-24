ec5fad38efd2511485388a5076b2e4b549e78172
ahmniab <ahmniab11@gmail.com>
2025-08-25 00:55:30 +0300

    Added List component

---
+++ src/components/List.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+
+type ListItemProps = {
+    onEdit: (newVal:string) => void, 
+    data:string
+}
+function ListItem(props:ListItemProps){
+    const [editing, setEditing] = useState<boolean>(false);
+    let [labelData, setLableData] = useState<string>(props.data);
+
+    return(
+    <li onDoubleClick={(e) => {setEditing(true)}}>
+        {editing?(
+            <>
+                <div>
+                    <input type="text" 
+                    value={labelData}
+                    onChange={(e)=> setLableData(e.target.value)}
+                    ></input>
+                    <button onClick={(e) => {setEditing(false)}}>ok</button>
+                </div>
+            </>
+            ):(
+                <p>{labelData}</p>
+            )}
+    </li>
+    );
+
+}
+type TodoList = Record<number, string>;
+function List() {
+    const [items, setItems] = useState<TodoList>({1:"thing1", 2:"thing2", 3:"thing3"});
+
+    return(
+        <>
+            <ol>
+                {Object.entries(items).map(([id, data]) =>{
+                    const itemId = Number(id);
+                    return(
+                    <ListItem 
+                        data={data} 
+                        onEdit={
+                            (newVal) => {
+                                items[itemId] = newVal;
+                                setItems(items);
+                            }
+                        }   
+                    />
+                    );
+                }
+                )
+            }
+            </ol>
+        </>
+    );
+}
+export default List;
