509d309a37ca1da31c0483d04b8e90da90e3ae67
ahmniab <ahmniab11@gmail.com>
2025-08-25 16:41:56 +0300

    remove logging in useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -4,18 +4,17 @@ function useLocalStorage<T>(key:string, initialValue:T)
     :[value:T, setValue: React.Dispatch<React.SetStateAction<T>>]{
     const [value, setValue] = useState<T>(() => {
         try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
         } catch (error) {
-        console.error(`Error reading localStorage key "${key}":`, error);
-        return initialValue;
+            console.error(`Error reading localStorage key "${key}":`, error);
+            return initialValue;
         }
     });
-    
+
     useEffect(() => {
         try {
             window.localStorage.setItem(key, JSON.stringify(value));
-            console.log('saved');
         } catch (error) {
             console.error('Error can\'t writ to localStorage');
         }
